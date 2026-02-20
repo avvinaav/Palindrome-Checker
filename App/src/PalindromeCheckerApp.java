c2511413a34209ec08f5af75153b015af9e61e30
avvinaav <rajavinav5544@gmail.com>
2026-02-20 10:03:20 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -4,32 +4,24 @@ import java.util.Stack;
 
 public class PalindromeCheckerApp{
     public static void main(String[] args) {
-        // 1. Hardcoded string
         String input = "noon";
-
-        // 2. Initialize Data Structures
-        // Note: Queue is an interface in Java, so we use LinkedList to implement it
         Stack<Character> stack = new Stack<>();
         Queue<Character> queue = new LinkedList<>();
 
-        // 3. Enqueue and Push characters
         for (int i = 0; i < input.length(); i++) {
             char c = input.charAt(i);
-            stack.push(c);   // Added to top
-            queue.add(c);    // Added to back (Enqueue)
+            stack.push(c);   
+            queue.add(c);   
         }
 
-        // 4. Logical Comparison (Dequeue vs Pop)
         boolean isPalindrome = true;
         while (!stack.isEmpty()) {
-            // pop() gets the LAST character, poll() gets the FIRST character
             if (stack.pop() != queue.poll()) {
                 isPalindrome = false;
                 break;
             }
         }
-
-        // 5. Print Result
+    
         if (isPalindrome) {
             System.out.println("Result: '" + input + "' is a palindrome.");
         } else {
