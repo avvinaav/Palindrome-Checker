51eda153983319ee5d007e43a5320188e028f3ec
avvinaav <rajavinav5544@gmail.com>
2026-02-20 10:02:05 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,18 +1,10 @@
 class PalindromeCheckerApp{
     public static void main(String[] args) {
-        // 1. Hardcoded string to check
         String original = "radar";
-        String reversed = ""; // Initializing an empty string for reversal
-
-        // 2. Loop (for loop) - Iterating backwards
-        // We start at the last index (length - 1) and go to index 0
+        String reversed = "";
         for (int i = original.length() - 1; i >= 0; i--) {
-            // 3. String Concatenation (+)
-            // This builds the reversed string character by character
             reversed = reversed + original.charAt(i);
         }
-
-        // 4. equals() Method - Compare content, not memory addresses
         if (original.equalsIgnoreCase(reversed)) {
             System.out.println("Result: '" + original + "' is a palindrome.");
         } else {
