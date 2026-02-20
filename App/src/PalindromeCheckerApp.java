2417ea06a38358d1c2f570768d02d33cc7e4326e
avvinaav <rajavinav5544@gmail.com>
2026-02-20 10:01:32 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,13 +1,7 @@
 class PalindromeCheckerApp{
     public static void main(String[] args) {
         String original = "madam";
-
-        // 2. Logic to reverse the string
-        // We use StringBuilder for easy string manipulation
         String reversed = new StringBuilder(original).reverse().toString();
-
-        // 3. Conditional Statement (if-else) to check palindrome
-        // Note: Use .equals() for string comparison, not ==
         if (original.equals(reversed)) {
             System.out.println("The word '" + original + "' is a palindrome.");
         } else {
